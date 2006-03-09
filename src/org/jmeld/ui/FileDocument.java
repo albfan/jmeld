185c1c120c079ad6a256a964a4d26f0717be9e2c
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2006-03-09 23:13:29 +0000

    A big performance improvement for equals() and hashCode() of Line.class

--- src/org/jmeld/ui/FileDocument.java
+++ src/org/jmeld/ui/FileDocument.java
@@ -36,6 +36,11 @@ public class FileDocument
     return file.getName();
   }
 
+  public File getFile()
+  {
+    return file;
+  }
+
   public PlainDocument getDocument()
   {
     return document;
@@ -253,11 +258,87 @@ public class FileDocument
 
       return getCharArray()[offset];
     }
+
+    public boolean equals(MyGapContent c2, int start1, int end1, int start2)
+    {
+      char[] array1;
+      char[] array2;
+      int    g1_0;
+      int    g1_1;
+      int    g2_0;
+      int    g2_1;
+      int    size;
+
+      array1 = getCharArray();
+      array2 = c2.getCharArray();
+
+      g1_0 = getGapStart();
+      g1_1 = getGapEnd();
+      g2_0 = c2.getGapStart();
+      g2_1 = c2.getGapEnd();
+
+      size = end1 - start1;
+      for (int o1 = start1, o2 = start2, i = 0; i < size; i++, o1++, o2++)
+      {
+        if (o1 == g1_0)
+        {
+          o1 += g1_1 - g1_0;
+        }
+
+        if (o2 == g2_0)
+        {
+          o2 += g2_1 - g2_0;
+        }
+
+        if (array1[o1] != array2[o2])
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    public int hashCode(int start, int end)
+    {
+      char[] array;
+      int    g0;
+      int    g1;
+      int    size;
+      int    h;
+
+      h = 0;
+
+      array = getCharArray();
+
+      g0 = getGapStart();
+      g1 = getGapEnd();
+
+      size = end - start;
+      for (int o = start, i = 0; i < size; i++, o++)
+      {
+        if (o == g0)
+        {
+          o += g1 - g0;
+        }
+
+        h = 31 * h + array[o];
+      }
+
+      if(h == 0)
+      {
+        h = 1;
+      }
+
+      return h;
+    }
+
   }
 
   public class Line
   {
     Element element;
+    int     hash;
 
     Line(Element element)
     {
@@ -282,35 +363,36 @@ public class FileDocument
 
     public boolean equals(Object o)
     {
-      Line         line2;
       Element      element2;
-      MyGapContent c1;
-      MyGapContent c2;
+      Line line2;
+      int          start1, start2;
+      int          end1, end2;
 
       line2 = ((Line) o);
       element2 = line2.element;
 
+      start1 = element.getStartOffset();
+      end1 = element.getEndOffset();
+      start2 = element2.getStartOffset();
+      end2 = element2.getEndOffset();
+
       // If the length is different the element is not equal!
-      if ((element.getEndOffset() - element.getStartOffset()) != (element2
-        .getEndOffset() - element2.getStartOffset()))
+      if ((end1 - start1) != (end2 - start2))
       {
         return false;
       }
 
-      c1 = content;
-      c2 = line2.getContent();
+      return content.equals(line2.getContent(), start1, end1, start2);
+    }
 
-      // Check char by char if they are equal.
-      for (int o1 = element.getStartOffset(), o2 = element2.getStartOffset();
-        o1 < element.getEndOffset(); o1++, o2++)
+    public int hashCode()
+    {
+      if (hash == 0)
       {
-        if (c1.getChar(o1) != c2.getChar(o2))
-        {
-          return false;
-        }
+        hash = content.hashCode(element.getStartOffset(), element.getEndOffset());
       }
 
-      return true;
+      return hash;
     }
 
     public String toString()
