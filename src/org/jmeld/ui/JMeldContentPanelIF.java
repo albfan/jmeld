6fdf30a61d655de406b0685e3aaefe190e1be24b
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2006-12-17 14:02:52 +0000

    Implemented framework to process keyevents (alt-up, alt-down, alt-right, alt-left)

---
+++ src/org/jmeld/ui/JMeldContentPanelIF.java
@@ -0,0 +1,24 @@
+package org.jmeld.ui;
+
+public interface JMeldContentPanelIF
+{
+  public boolean isSaveEnabled();
+
+  public void doSave();
+
+  public boolean isUndoEnabled();
+
+  public void doUndo();
+
+  public boolean isRedoEnabled();
+
+  public void doRedo();
+
+  public void doLeft();
+
+  public void doRight();
+
+  public void doUp();
+
+  public void doDown();
+}
