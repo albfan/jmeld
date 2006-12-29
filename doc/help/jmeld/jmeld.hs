f2090bbb120071818fe63449a04edc190434d004
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2006-12-29 13:44:57 +0000

    created helpset for javahelp

---
+++ doc/help/jmeld/jmeld.hs
@@ -0,0 +1,18 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+  <title>JMeld</title>
+  <maps>
+    <mapref location="Map.jhm"/>
+    <homeID>overview</homeID>
+  </maps>
+  <view>
+    <name>TOC</name>
+    <label>TOC</label>
+    <type>javax.help.TOCView</type>
+    <data>toc.xml</data>
+  </view>
+</helpset>
