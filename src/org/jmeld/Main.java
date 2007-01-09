d60c8b829f3e1ffda189a1c3f34697239cd1a768
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2007-01-09 19:35:11 +0000

    added LGPL License text

--- src/org/jmeld/Main.java
+++ src/org/jmeld/Main.java
@@ -1,3 +1,19 @@
+/*
+   JMeld is a visual diff and merge tool.
+   Copyright (C) 2007  Kees Kuip
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA  02110-1301  USA
+ */
 package org.jmeld;
 
 public class Main
@@ -5,6 +21,7 @@ public class Main
   public static void main(String[] args)
   {
     //JMeld.main(new String[] { "files/added1.xml", "files/added2.xml"} );
-    JMeld.main(new String[] { "files/analyze.1.15.c", "files/analyze.1.25.c" });
+    //JMeld.main(new String[] { "files/analyze.1.15.c", "files/analyze.1.25.c" });
+    JMeld.main(new String[] { "files1", "files2" });
   }
 }
