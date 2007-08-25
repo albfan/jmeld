93c88834d19bd9801eba69cc8c5e1564245df24b
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2007-08-25 13:37:17 +0000

    +compare arrays of chars with ignorewhitespace

--- src/org/jmeld/util/file/CompareUtil.java
+++ src/org/jmeld/util/file/CompareUtil.java
@@ -22,6 +22,7 @@ import org.jmeld.util.node.*;
 import java.io.*;
 import java.nio.*;
 import java.nio.channels.*;
+import java.nio.charset.*;
 import java.util.*;
 
 public class CompareUtil
@@ -44,6 +45,7 @@ public class CompareUtil
     return false;
   }
 
+/*
   private static boolean contentEquals2(
     FileNode nodeLeft,
     FileNode nodeRight,
@@ -61,8 +63,17 @@ public class CompareUtil
       return false;
     }
   }
+  */
 
   private static boolean contentEquals(
+    FileNode nodeLeft,
+    FileNode nodeRight,
+    boolean  ignoreWhitespace)
+  {
+    return contentEquals1(nodeLeft, nodeRight, ignoreWhitespace);
+  }
+
+  private static boolean contentEquals1(
     FileNode nodeLeft,
     FileNode nodeRight,
     boolean  ignoreWhitespace)
@@ -124,6 +135,10 @@ public class CompareUtil
           while (bbLeft.hasRemaining())
           {
             leftByte = bbLeft.get();
+            // 0x0A = LINE FEED
+            // 0x0C = FORM FEED
+            // 0x0D = CARRIAGE RETURN
+            // 0x20 = SPACE
             if (leftByte == 0x0A || leftByte == 0x0C || leftByte == 0x0D
               || leftByte == 0x20)
             {
@@ -206,5 +221,240 @@ public class CompareUtil
         ex.printStackTrace();
       }
     }
+  }
+
+  private static boolean contentEquals2(
+    FileNode nodeLeft,
+    FileNode nodeRight,
+    boolean  ignoreWhitespace)
+  {
+    File             fileLeft;
+    File             fileRight;
+    RandomAccessFile fLeft;
+    RandomAccessFile fRight;
+    FileChannel      fcLeft;
+    FileChannel      fcRight;
+    ByteBuffer       bbLeft;
+    ByteBuffer       bbRight;
+    Charset          charset;
+    CharBuffer       cbLeft;
+    CharBuffer       cbRight;
+    boolean          equals;
+    boolean          leftFound;
+    boolean          rightFound;
+    char             leftChar;
+    char             rightChar;
+
+    fLeft = null;
+    fRight = null;
+
+    try
+    {
+      fileLeft = nodeLeft.getFile();
+      fileRight = nodeRight.getFile();
+
+      if (fileLeft.isDirectory() || fileRight.isDirectory())
+      {
+        return true;
+      }
+
+      if (!ignoreWhitespace && fileLeft.length() != fileRight.length())
+      {
+        return false;
+      }
+
+      fLeft = new RandomAccessFile(fileLeft, "r");
+      fRight = new RandomAccessFile(fileRight, "r");
+      fcLeft = fLeft.getChannel();
+      fcRight = fRight.getChannel();
+
+      bbLeft = fcLeft.map(FileChannel.MapMode.READ_ONLY, 0, (int) fcLeft.size());
+      bbRight = fcRight.map(FileChannel.MapMode.READ_ONLY, 0,
+          (int) fcRight.size());
+
+      if (!ignoreWhitespace)
+      {
+        equals = bbLeft.equals(bbRight);
+      }
+      else
+      {
+        charset = Charset.defaultCharset();
+        cbLeft = charset.decode(bbLeft);
+        cbRight = charset.decode(bbRight);
+
+        equals = false;
+        leftChar = 0;
+        rightChar = 0;
+        for (;;)
+        {
+          leftFound = false;
+          while (cbLeft.hasRemaining())
+          {
+            leftChar = cbLeft.get();
+            if (Character.isWhitespace(leftChar))
+            {
+              continue;
+            }
+
+            leftFound = true;
+            break;
+          }
+
+          rightFound = false;
+          while (cbRight.hasRemaining())
+          {
+            rightChar = cbRight.get();
+            if (Character.isWhitespace(rightChar))
+            {
+              continue;
+            }
+
+            rightFound = true;
+            break;
+          }
+
+          if (leftFound && rightFound)
+          {
+            if (leftChar == rightChar)
+            {
+              continue;
+            }
+
+            equals = false;
+            break;
+          }
+
+          if ((leftFound && !rightFound) || (!leftFound && rightFound))
+          {
+            equals = false;
+            break;
+          }
+
+          if (!leftFound && !rightFound)
+          {
+            equals = true;
+            break;
+          }
+        }
+      }
+
+      return equals;
+    }
+    catch (Exception ex)
+    {
+      ex.printStackTrace();
+      return false;
+    }
+    finally
+    {
+      try
+      {
+        if (fLeft != null)
+        {
+          fLeft.close();
+        }
+      }
+      catch (Exception ex)
+      {
+        ex.printStackTrace();
+      }
+
+      try
+      {
+        if (fRight != null)
+        {
+          fRight.close();
+        }
+      }
+      catch (Exception ex)
+      {
+        ex.printStackTrace();
+      }
+    }
+  }
+
+  public static boolean contentEquals(
+    char[]  left,
+    char[]  right,
+    boolean ignoreWhitespace)
+  {
+    boolean equals;
+    boolean leftFound;
+    boolean rightFound;
+    char    leftChar;
+    char    rightChar;
+    int     leftIndex;
+    int     rightIndex;
+
+    leftIndex = 0;
+    rightIndex = 0;
+
+    if (!ignoreWhitespace)
+    {
+      equals = Arrays.equals(left, right);
+    }
+    else
+    {
+      equals = false;
+      leftChar = 0;
+      rightChar = 0;
+      for (;;)
+      {
+        leftFound = false;
+        while (leftIndex < left.length)
+        {
+          leftChar = left[leftIndex];
+          leftIndex++;
+
+          if (Character.isWhitespace(leftChar))
+          {
+            continue;
+          }
+
+          leftFound = true;
+          break;
+        }
+
+        rightFound = false;
+        while (rightIndex < right.length)
+        {
+          rightChar = right[rightIndex];
+          rightIndex++;
+
+          if (Character.isWhitespace(rightChar))
+          {
+            continue;
+          }
+
+          rightFound = true;
+          break;
+        }
+
+        if (leftFound && rightFound)
+        {
+          if (leftChar == rightChar)
+          {
+            continue;
+          }
+
+          equals = false;
+          break;
+        }
+
+        if ((leftFound && !rightFound) || (!leftFound && rightFound))
+        {
+          equals = false;
+          break;
+        }
+
+        if (!leftFound && !rightFound)
+        {
+          equals = true;
+          break;
+        }
+      }
+    }
+
+    return equals;
   }
 }
