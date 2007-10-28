16ecb8d1871a7f96eba1c17e21eafc72069c2a76
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2007-10-28 14:11:23 +0000

    new

---
+++ src/org/jmeld/util/RegexUtil.java
@@ -0,0 +1,26 @@
+package org.jmeld.util;
+
+import java.util.regex.*;
+
+public class RegexUtil
+{
+  private static Pattern whiteSpacePattern;
+
+  public static String replaceAll(
+    Pattern pattern,
+    String  string,
+    String  replacement)
+  {
+    return pattern.matcher(string).replaceAll(replacement);
+  }
+
+  public static Pattern getWhitespacePattern()
+  {
+    if (whiteSpacePattern == null)
+    {
+      whiteSpacePattern = Pattern.compile("^\\p{Blank}+");
+    }
+
+    return whiteSpacePattern;
+  }
+}
---
+++ src/org/jmeld/util/Result.java
@@ -0,0 +1,84 @@
+package org.jmeld.util;
+
+import java.io.*;
+import java.util.*;
+
+public class Result
+{
+  // instance variables
+  private boolean   result;
+  private String    description = "";
+  private Exception exception;
+
+  private Result(
+    boolean   result,
+    String    description,
+    Exception exception)
+  {
+    this.result = result;
+    this.description = description;
+    this.exception = exception;
+  }
+
+  public static Result TRUE()
+  {
+    return new Result(true, null, null);
+  }
+
+  public static Result FALSE(String description)
+  {
+    return new Result(false, description, null);
+  }
+
+  public static Result FALSE(
+    String    description,
+    Exception ex)
+  {
+    return new Result(false, description, ex);
+  }
+
+  public boolean isTrue()
+  {
+    return getResult();
+  }
+
+  public boolean isFalse()
+  {
+    return !getResult();
+  }
+
+  public boolean getResult()
+  {
+    return result;
+  }
+
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  public String getDescription()
+  {
+    if (description == null)
+    {
+      return "";
+    }
+
+    return description.toString();
+  }
+
+  public boolean hasException()
+  {
+    return exception != null;
+  }
+
+  public Exception getException()
+  {
+    return exception;
+  }
+
+  public String toString()
+  {
+    return result + " :" + description;
+  }
+}
