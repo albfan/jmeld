14aa865dcc44bd167364a4914c4d94f37f05ac39
keeskuip <keeskuip@1a690e68-620e-0410-a9b5-9281efe46c0f>
2008-09-24 19:14:05 +0000

    formatting

--- src/org/jmeld/ui/swing/table/JMTreeTable.java
+++ src/org/jmeld/ui/swing/table/JMTreeTable.java
@@ -17,31 +17,12 @@
 package org.jmeld.ui.swing.table;
 
 import org.jdesktop.swingx.*;
-import org.jdesktop.swingx.decorator.*;
-import org.jdesktop.swingx.treetable.*;
-import org.jmeld.*;
-import org.jmeld.diff.*;
-import org.jmeld.ui.search.*;
-import org.jmeld.ui.swing.*;
-import org.jmeld.ui.text.*;
-import org.jmeld.ui.util.*;
-import org.jmeld.util.*;
-import org.jmeld.util.file.*;
-import org.jmeld.util.node.*;
 
-import javax.swing.*;
-import javax.swing.event.*;
 import javax.swing.table.*;
-import javax.swing.text.*;
-import javax.swing.undo.*;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.util.*;
 
 public class JMTreeTable
-       extends JXTreeTable
+    extends JXTreeTable
 {
   private int                charWidth;
   private TreeTableHackerExt myTreeTableHacker;
@@ -55,10 +36,10 @@ public class JMTreeTable
 
   public void setTreeTableModel(JMTreeTableModel tableModel)
   {
-    TableColumnModel  columnModel;
-    TableColumn       column;
-    int               preferredWidth;
-    TableCellEditor   editor;
+    TableColumnModel cModel;
+    TableColumn column;
+    int preferredWidth;
+    TableCellEditor editor;
     TableCellRenderer renderer;
 
     super.setTreeTableModel(tableModel);
@@ -70,16 +51,19 @@ public class JMTreeTable
      */
     if (tableModel != null)
     {
+      //TODO[KKU]: Dit werkt niet meer. Zowel JXTable als JTable proberen
+      //           de preferred size te berekenen. ouch.
+
       // Make sure the icons fit well.
       if (getRowHeight() < 22)
       {
         setRowHeight(22);
       }
 
-      columnModel = getColumnModel();
+      cModel = getColumnModel();
       for (int i = 0; i < tableModel.getColumnCount(); i++)
       {
-        column = columnModel.getColumn(i);
+        column = cModel.getColumn(i);
         renderer = tableModel.getRenderer(i);
         if (renderer != null)
         {
@@ -107,18 +91,19 @@ public class JMTreeTable
     }
   }
 
+  @Override
   protected TreeTableHacker getTreeTableHacker()
   {
     if (myTreeTableHacker == null)
     {
       myTreeTableHacker = new TreeTableHackerExt()
-          {
-            @Override
-            protected boolean isHitDetectionFromProcessMouse()
-            {
-              return false;
-            }
-          };
+      {
+        @Override
+        protected boolean isHitDetectionFromProcessMouse()
+        {
+          return false;
+        }
+      };
     }
 
     return myTreeTableHacker;
